4732bae8b505d080f185e3d2f5a06be5a3f98630
Bruno da Silva Perez Araújo <BrunoKellyAraujo@gmail.com>
2022-01-21 17:06:21 -0300

    First Commit

--- src/main/java/br/opengl/ch1_getting_started/ch1_02_hello_triangle/HelloTriangle_Indexed.java
+++ src/main/java/br/opengl/ch1_getting_started/ch1_02_hello_triangle/HelloTriangle_Indexed.java
@@ -9,19 +9,6 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 final class HelloTriangle_Indexed {
 
-    private static final float[] VERTICES = {
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left
-    };
-
-    private static final int[] INDICES = { // Note that we start from 0!
-            0, 1, 3,  // first Triangle
-            1, 2, 3   // second Triangle
-    };
-
-
     public static void main (String[] args) {
         OpenGL.bytebudy();
 
